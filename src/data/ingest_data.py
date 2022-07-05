2517f88fabf712bd37e9288ad9489632c5dabb66
scdiaza <98289937+scdiaza@users.noreply.github.com>
2022-07-04 22:28:59 -0500

    Update ingest_data.py

--- src/data/ingest_data.py
+++ src/data/ingest_data.py
@@ -28,7 +28,7 @@ def ingest_data():
     html = requests.get("https://github.com/jdvelasq/datalabs/blob/master/datasets/precio_bolsa_nacional/xls/")
     doc = lxml.html.fromstring(html.content)
     urls=doc.xpath("//a/text()")
-    print(urls)
+    #print(urls)
     
     #
     os.chdir(os.path.join("data_lake", "landing" )) 
@@ -41,7 +41,7 @@ def ingest_data():
             
             #
             response = requests.get(file_url)
-            open( probably_a_url_to_xlsx_file, "wb").write(response.content)
+            open( probably_a_url_to_xlsx_file[:-1], "wb").write(response.content)
 #ingest_data()
 
 if __name__ == "__main__":
