9d5ee6b73d9fd945f4041e7d6d1d2bb3c9f984af
scdiaza <98289937+scdiaza@users.noreply.github.com>
2022-07-04 21:22:47 -0500

    Update make_daily_prices_plot.py

--- src/visualization/make_daily_prices_plot.py
+++ src/visualization/make_daily_prices_plot.py
@@ -7,7 +7,40 @@ def make_daily_prices_plot():
     El archivo se debe salvar en formato PNG en data_lake/business/reports/figures/daily_prices.png.
 
     """
-    raise NotImplementedError("Implementar esta función")
+    #
+    # 
+    import os
+    import pandas as pd
+    import plotly.express as px 
+    import matplotlib.pyplot as plt
+    
+    #
+    # (navegar a raiz del proyecto)
+    #
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    os.chdir(dir_path)
+    os.chdir("..")
+    os.chdir("..")
+    
+    #
+    # (leer precios diarios)
+    #
+    daily_prices=pd.read_csv(os.path.join("data_lake","business", "precios-diarios.csv" ),header=0)
+    #return(daily_prices)
+    #
+    # (grafico de linea precios diarios)
+    #
+    #
+    plt.figure(figsize=(24,18))
+    #
+    plot = daily_prices.plot(x="fecha" , y=["precio"], kind="line")
+    #
+    plt.tick_params(axis='x',labelsize=15,rotation=45)
+    #plt.tight_layout()
+    #
+    plt.savefig(os.path.join("data_lake","business","reports","figures","daily_prices.png" ))
+    
+print(make_daily_prices_plot())
 
 
 if __name__ == "__main__":
