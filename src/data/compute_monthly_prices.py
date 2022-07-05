b8b22b2540b1ab17653490df6f382e902718b919
scdiaza <98289937+scdiaza@users.noreply.github.com>
2022-07-05 00:29:20 -0500

    Update compute_monthly_prices.py

--- src/data/compute_monthly_prices.py
+++ src/data/compute_monthly_prices.py
@@ -19,10 +19,10 @@ def compute_monthly_prices():
     #
     # (navegar a raiz del proyecto)
     #
-    dir_path = os.path.dirname(os.path.realpath(__file__))
-    os.chdir(dir_path)
-    os.chdir("..")
-    os.chdir("..")
+    #dir_path = os.path.dirname(os.path.realpath(__file__))
+    #os.chdir(dir_path)
+    #os.chdir("..")
+    #os.chdir("..")
     
     #
     # (leer precios diarios)
