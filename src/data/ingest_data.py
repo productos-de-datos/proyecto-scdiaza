0bc0dea39a2a7bd7dfe88ce5a4883ea99f84d0ac
scdiaza <98289937+scdiaza@users.noreply.github.com>
2022-07-04 21:16:05 -0500

    Update ingest_data.py

--- src/data/ingest_data.py
+++ src/data/ingest_data.py
@@ -13,8 +13,36 @@ def ingest_data():
     descarga debe realizarse usando únicamente funciones de Python.
 
     """
-    raise NotImplementedError("Implementar esta función")
-
+    #
+    import os
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    os.chdir(dir_path)
+    os.chdir("..")
+    os.chdir("..")
+    print(os.getcwd())
+    #
+    import requests
+    os.system("pip install  lxml")
+    import lxml
+    #
+    html = requests.get("https://github.com/jdvelasq/datalabs/blob/master/datasets/precio_bolsa_nacional/xls/")
+    doc = lxml.html.fromstring(html.content)
+    urls=doc.xpath("//a/text()")
+    print(urls)
+    
+    #
+    os.chdir(os.path.join("data_lake", "landing" )) 
+    for probably_a_url_to_xlsx_file in urls:
+        import re
+        
+        if re.search("xlsx$", probably_a_url_to_xlsx_file):
+            file_url = "https://github.com/jdvelasq/datalabs/blob/master/datasets/precio_bolsa_nacional/xls/{}?raw=true".format(probably_a_url_to_xlsx_file)
+            #print(file_url)
+            
+            #
+            response = requests.get(file_url)
+            open( probably_a_url_to_xlsx_file, "wb").write(response.content)
+ingest_data()
 
 if __name__ == "__main__":
     import doctest
