1711cadfcc97222542844aac46f5afcb33594f32
scdiaza <98289937+scdiaza@users.noreply.github.com>
2022-07-04 21:12:26 -0500

    Update compute_daily_prices.py

--- src/data/compute_daily_prices.py
+++ src/data/compute_daily_prices.py
@@ -12,10 +12,36 @@ def compute_daily_prices():
 
 
     """
-    raise NotImplementedError("Implementar esta función")
+    #
+    import os
+    import pandas as pd
+    import numpy as np
+    
+    #
+    # (navegar a raiz del proyecto)
+    #
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    os.chdir(dir_path)
+    os.chdir("..")
+    os.chdir("..")
+    
+    #
+    # (leer precios diarios)
+    #
+    hourly_prices=pd.read_csv(os.path.join("data_lake","cleansed","precios-horarios.csv" ),header=0)
+    
+    #
+    # (loop sobre las fechas)
+    #
+    an=hourly_prices.groupby('fecha').mean()
+    an.drop(columns="hora", inplace=True)
+    
+    #
+    an.to_csv( os.path.join("data_lake","business", "precios-diarios.csv"))
+
 
 
 if __name__ == "__main__":
     import doctest
-
+    compute_daily_prices()
     doctest.testmod()
