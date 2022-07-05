601ab8e26bd6c21d18fcb5f8b47e126f7d6b3b3c
scdiaza <98289937+scdiaza@users.noreply.github.com>
2022-07-05 00:02:49 -0500

    Update pipeline.py

--- src/data/pipeline.py
+++ src/data/pipeline.py
@@ -1,81 +1,72 @@
 """
-Construya un pipeline de Luigi que:
+Módulo de orquestamiento con luigui.
+-------------------------------------------------------------------------------
+"""
 
+
+
+"""
+Construya un pipeline de Luigi que:
 * Importe los datos xls
 * Transforme los datos xls a csv
 * Cree la tabla unica de precios horarios.
 * Calcule los precios promedios diarios
 * Calcule los precios promedios mensuales
-
 En luigi llame las funciones que ya creo.
-
 """
-
-import os
-#
-# (navegar a raiz del proyecto)
-#
-dir_path = os.path.dirname(os.path.realpath(__file__))
-os.chdir(dir_path)
-os.chdir("..")
-os.chdir("..")
-os.chdir(os.path.join("src", "data"))
-print(os.getcwd())
-
-
-#
 import luigi
 from luigi import Task, LocalTarget
 import os
 import pandas as pd
-#
 from ingest_data import ingest_data
 from transform_data import transform_data
-from  clean_data import clean_data
-from compute_daily_prices import compute_daily_prices
+from clean_data import clean_data
 from compute_monthly_prices import compute_monthly_prices
+from compute_daily_prices import compute_daily_prices
 
-
-#
-#
-class pipeline_until_clean_data(Task):
-    
+class Ingest_Transform_Clean_Data(Task):
     def output(self):
-        return LocalTarget(os.path.join("data_lake","cleansed","precios-horarios.csv" ))
+        return LocalTarget("data_lake/cleansed/precios-horarios.csv")
+
     def run(self):
         ingest_data()
+        print("ingest_data-----------------OK")
         transform_data()
+        print("transform_data----------OK")
         clean_data()
-#
-#
-class Compute_daily_prices(Task):
+        print("clean_data-----------OK")
+
+class compute_daily_prices(Task):
     def requires (self):
-        return pipeline_until_clean_data()
+        return Ingest_Transform_Clean_Data()
+
     def output(self):
-        return LocalTarget(os.path.join("data_lake","business","precios-diarios.csv"))
+        return LocalTarget('data_lake/business/precios-diarios.csv')
+
     def run(self):
         compute_daily_prices()
-#
-class Compute_monthly_prices(Task):
-    def requires (self):
-        return pipeline_until_clean_data()
+        print("compute_daily_prices------------OK")
+
+class compute_monthly_prices(Task):
+    def requires(self):
+        return Ingest_Transform_Clean_Data()
+
     def output(self):
-        return LocalTarget(os.path.join("data_lake","business","precios-mensuales.csv"))
+        return LocalTarget('data_lake/business/precios-mensuales.csv')
+
     def run(self):
         compute_monthly_prices()
+        print("compute_monthly_prices-------------------OK")
 
-#
-class Pipeline(Task):
+class Pipe_Run_All(Task):
 
     def requires(self):
         return [
-            Compute_daily_prices(),
-            Compute_monthly_prices(),
+            compute_daily_prices(),
+            compute_monthly_prices(),
         ]
 
-#
-#
 if __name__ == "__main__":
     import doctest
-    luigi.run(['Pipeline','--local-scheduler'])
+    luigi.run(['Pipe_Run_All','--local-scheduler'])
     doctest.testmod()
